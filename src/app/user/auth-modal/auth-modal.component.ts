8a71cb8d4ca7bf67cb7b204bd3540080f010f356
Leandro Teixeira <leandroteixeira3@gmail.com>
2022-06-03 08:07:06 -0300

    feat: auth-modal component

--- src/app/user/auth-modal/auth-modal.component.ts
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -1,15 +1,13 @@
+/* eslint-disable import/prefer-default-export */
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-auth-modal',
   templateUrl: './auth-modal.component.html',
-  styleUrls: ['./auth-modal.component.css']
+  styleUrls: ['./auth-modal.component.css'],
 })
 export class AuthModalComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
