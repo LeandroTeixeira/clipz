3c66f1609517842fba2e057811c01a7517335747
Leandro Teixeira <leandroteixeira3@gmail.com>
2022-06-30 20:52:43 -0300

    feat: tabs element added to html

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,35 +1,36 @@
-import { Component, AfterContentInit, ContentChildren, QueryList } from '@angular/core';
+/* eslint-disable no-param-reassign */
+/* eslint-disable import/prefer-default-export */
+import {
+  Component,
+  AfterContentInit,
+  ContentChildren,
+  QueryList,
+} from '@angular/core';
 import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
-  styleUrls: ['./tabs-container.component.css']
+  styleUrls: ['./tabs-container.component.css'],
 })
 export class TabsContainerComponent implements AfterContentInit {
-
-  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList()
-
-  constructor() { }
+  // eslint-disable-next-line operator-linebreak
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> =
+    new QueryList();
 
   ngAfterContentInit(): void {
-    const activeTabs = this.tabs?.filter(
-      tab => tab.active
-    )
+    const activeTabs = this.tabs?.filter((tab) => tab.active);
 
     if (!activeTabs || activeTabs.length === 0) {
-      this.selectTab(this.tabs!.first)
+      this.selectTab(this.tabs!.first);
     }
   }
 
   selectTab(tab: TabComponent) {
-    this.tabs?.forEach(tab => {
-      tab.active = false
-    })
-
-    tab.active = true
-
-    return false
+    this.tabs?.forEach((t) => {
+      t.active = false;
+    });
+    tab.active = true;
+    return false;
   }
-
 }
